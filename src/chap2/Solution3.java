d6eb763246a96cac952ef62847ce5c45d66bf742
xxxknight <xingshiyang24@163.com>
2016-03-04 19:58:09 +0800

    d

--- src/chap2/Solution3.java
+++ src/chap2/Solution3.java
@@ -1,10 +1,10 @@
 package chap2;
 
 /**
+ * 二维数组中的查找
  * 题目描述
  * 
- * 在一个二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。
- * 请完成一个函数，输入这样的一个二维数组和一个整数，
+ * 在一个二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。 请完成一个函数，输入这样的一个二维数组和一个整数，
  * 判断数组中是否含有该整数。 输入描述: array： 待查找的二维数组 target：查找的数字
  * 
  * 
@@ -12,8 +12,7 @@ package chap2;
  * 
  * tag: 二分查找
  * 
- * @author xxxknight
- *
+ * 
  */
 public class Solution3 {
 	public boolean Find(int[][] array, int target) {
@@ -22,13 +21,20 @@ public class Solution3 {
 		}
 		int row = array.length;
 		int col = array[0].length;
+
+		int x = 0;
+		int y = col - 1;
+		while (x < row && y >= 0) {
+			if (target == array[x][y]) {
+				return true;
+			} else if (target > array[x][y]) {
+				x++;
+			} else {
+				y--;
+			}
+
+		}
 		return false;
 
 	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
 }
