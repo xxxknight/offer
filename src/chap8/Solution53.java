08d25e0ca000e738cf4c6375fb480c0508370ed3
xxxknight <xingshiyang24@163.com>
2016-03-14 12:14:07 +0800

    67

--- src/chap8/Solution53.java
+++ src/chap8/Solution53.java
@@ -14,7 +14,62 @@ package chap8;
 
 public class Solution53 {
 	public boolean match(char[] str, char[] pattern) {
+		if (str == null || pattern == null) {
+			return false;
+		}
 
+		return matchCore(str, 0, pattern, 0);
+	}
+
+	private static boolean matchCore(char[] str, int i, char[] pattern, int p) {
+
+		// 匹配串和模式串都到达尾，说明成功匹配
+		if (i >= str.length && p >= pattern.length) {
+			return true;
+		}
+
+		// 只有模式串到达结尾，说明匹配失败
+		if (i != str.length && p >= pattern.length) {
+			return false;
+		}
+
+		// 模式串未结束，匹配串有可能结束有可能未结束
+
+		// p位置的下一个字符中为*号
+		if (p + 1 < pattern.length && pattern[p + 1] == '*') {
+
+			// 匹配串已经结束
+			if (i >= str.length) {
+				return matchCore(str, i, pattern, p + 2);
+			}
+			// 匹配串还没有结束
+			else {
+				if (pattern[p] == str[i] || pattern[p] == '.') {
+					return
+					// 匹配串向后移动一个位置，模式串向后移动两个位置
+					matchCore(str, i + 1, pattern, p + 2)
+					// 匹配串向后移动一个位置，模式串不移动
+							|| matchCore(str, i + 1, pattern, p)
+							// 匹配串不移动，模式串向后移动两个位置
+							|| matchCore(str, i, pattern, p + 2);
+				} else {
+					return matchCore(str, i, pattern, p + 2);
+				}
+			}
+		}
+
+		// 匹配串已经结束
+		if (i >= str.length) {
+			return false;
+		}
+		// 匹配串还没有结束
+		else {
+			if (str[i] == pattern[p] || pattern[p] == '.') {
+				return matchCore(str, i + 1, pattern, p + 1);
+			}
+		}
+
+		return false;
 	}
 
 }
--- src/chap8/Solution54.java
+++ src/chap8/Solution54.java
@@ -1,10 +1,30 @@
 package chap8;
 
 /**
+ * 题目描述
+ * 
+ * 请实现一个函数用来判断字符串是否表示数值（包括整数和小数）。例如，字符串"+100","5e2","-123","3.1416"和"-1E-16"
+ * 都表示数值。 但是"12e","1a3.14","1.2.3","+-5"和"12e+4.3"都不是。
  * 
  * @author founder
- *
+ * 
  */
 public class Solution54 {
-
+	public boolean isNumeric(char[] str) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < str.length; i++) {
+			sb.append(str[i]);
+		}
+		String s = sb.toString();
+		s = s.trim();
+		if (s == null || s.length() == 0) {
+			return false;
+		}
+		String regex = "[-+]?(\\d+\\.?|\\.\\d+)\\d*([e|E][-+]?\\d+)?";
+		if (s.matches(regex)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
